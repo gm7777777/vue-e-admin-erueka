9b313fb75d83d669b8e3a7074d159b4a71bc6cf1
gm <119618949@qq.com>
2020-11-09 15:30:57 +0800

    first

---
+++ src/main/java/com/gm/webadmin/erueka/EruekaApplication.java
@@ -0,0 +1,16 @@
+package com.gm.webadmin.erueka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EruekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EruekaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gm/webadmin/erueka/EruekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gm.webadmin.erueka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EruekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
